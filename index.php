fda7eb990bad3e1a7e1b9f45204f3e1e7d6451be
Kevin 'Sumpheus' Nguma <51791578+Sumpheus@users.noreply.github.com>
2020-06-02 16:58:40 +0200

    Début

---
+++ index.php
@@ -0,0 +1,21 @@
+<<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href='style.css' rel="stylesheet">
+    <title></title>
+  </head>
+  <body>
+<div='conteneur1'>
+<h1>Explorateur Perso PHP</h1>
+<div='contenuphp'>
+  <?php
+  $file='D:\Logiciels\wamp64\www\KevinNguma\ACS\Testphpbla\salut';
+  $filesize=($salut);
+  $filetype=filetype($salut);
+  echo 'the size of the file named salut is $filesize bytes';
+  ?>
+</div>
+</div>
+  </body>
+</html>
